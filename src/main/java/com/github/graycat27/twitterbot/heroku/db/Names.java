d536abeebc1fb2797c2bb5768ce8726f6b8c989b
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-07-27 21:18:22 +0900

    namespaceがxmlとjavaで相違していたのを訂正

--- src/main/java/com/github/graycat27/twitterbot/heroku/db/Names.java
+++ src/main/java/com/github/graycat27/twitterbot/heroku/db/Names.java
@@ -6,8 +6,8 @@ public class Names {
         private static final String mybatisNameSpace = "twitterBot.";
 
         public static final String botUsers = mybatisNameSpace+"BotUserMapper";
-        public static final String twitterAuth = mybatisNameSpace+"TwitterAuth";
-        public static final String twitterRecord = mybatisNameSpace+"TwitterRecord";
+        public static final String twitterAuth = mybatisNameSpace+"TwitterAuthMapper";
+        public static final String twitterRecord = mybatisNameSpace+"TwitterRecordMapper";
     }
 
 }
