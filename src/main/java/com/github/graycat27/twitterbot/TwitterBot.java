b722e23a6951dcd4c1bbc091b371d80c862bb2e3
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-07-27 13:56:30 +0900

    add start-end logging

--- src/main/java/com/github/graycat27/twitterbot/TwitterBot.java
+++ src/main/java/com/github/graycat27/twitterbot/TwitterBot.java
@@ -5,9 +5,9 @@ import com.github.graycat27.twitterbot.bot.BotTask;
 public class TwitterBot {
 
     public static void main(String[] args){
-
+        System.out.println("Bot Start ----->");
         BotTask task = new BotTask();
         task.run();
-
+        System.out.println("Bot End -----<");
     }
 }
