cc0f568bee14bd58529b9ccd813ec1c1ca1363c3
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-07-27 13:16:14 +0900

    remove unused Query defines

--- src/main/java/com/github/graycat27/twitterbot/heroku/db/sql/define/ChangeQuery.java
+++
@@ -1,4 +0,0 @@
-package com.github.graycat27.twitterbot.heroku.db.sql.define;
-
-public interface ChangeQuery extends SqlQuery {
-}
--- src/main/java/com/github/graycat27/twitterbot/heroku/db/sql/define/SqlQuery.java
+++
@@ -1,7 +0,0 @@
-package com.github.graycat27.twitterbot.heroku.db.sql.define;
-
-public interface SqlQuery {
-
-    /** return the sql query string */
-    String sql();
-}
