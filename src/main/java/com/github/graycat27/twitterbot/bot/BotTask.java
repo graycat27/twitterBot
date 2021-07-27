c0cc8cdd03b1dcf7751e91cbbae9851d4d8d66ef
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-07-27 20:25:25 +0900

    taskの更新（mybatisの各table読み取り実験）

--- src/main/java/com/github/graycat27/twitterbot/bot/BotTask.java
+++ src/main/java/com/github/graycat27/twitterbot/bot/BotTask.java
@@ -2,7 +2,11 @@ package com.github.graycat27.twitterbot.bot;
 
 import com.github.graycat27.twitterbot.heroku.db.domain.BotUsersDomain;
 import com.github.graycat27.twitterbot.heroku.db.domain.IDbDomain;
+import com.github.graycat27.twitterbot.heroku.db.domain.TwitterAuthDomain;
+import com.github.graycat27.twitterbot.heroku.db.domain.TwitterRecordDomain;
 import com.github.graycat27.twitterbot.heroku.db.query.BotUserQuery;
+import com.github.graycat27.twitterbot.heroku.db.query.TwitterAuthQuery;
+import com.github.graycat27.twitterbot.heroku.db.query.TwitterRecordQuery;
 import com.github.graycat27.twitterbot.utils.ListUtil;
 
 import java.util.List;
@@ -21,6 +25,13 @@ public class BotTask {
         List<BotUsersDomain> resultList = query.selectMulti(null);
         ListUtil.printList(resultList);
 
+        TwitterAuthQuery authQuery = new TwitterAuthQuery();
+        TwitterAuthDomain authResult = authQuery.selectOne(null);
+        System.out.println(authResult);
+
+        TwitterRecordQuery recordQuery = new TwitterRecordQuery();
+        List<TwitterRecordDomain> recordResult = recordQuery.selectMulti(null);
+        ListUtil.printList(recordResult);
 
     }
 }
