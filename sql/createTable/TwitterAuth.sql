6b3cc828774bf8c83afe47cf92d8ecc6c195b988
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-07-27 21:18:04 +0900

    snakeに列名変更

--- sql/createTable/TwitterAuth.sql
+++ sql/createTable/TwitterAuth.sql
@@ -2,7 +2,7 @@ CREATE TABLE TWITTER_AUTH (
     created_date timestamp DEFAULT CURRENT_TIMESTAMP,
     update_date timestamp,
     is_deleted boolean DEFAULT false,
-    apikey text,
+    api_key text,
     secret_key text,
     bearer_token text
 )
