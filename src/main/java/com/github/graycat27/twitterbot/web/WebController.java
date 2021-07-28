0df47f5ef6976f5da9826687f787e2dcb72a3909
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-07-28 18:20:23 +0900

    不使用importの削除とjavaDocコメントの整備

--- src/main/java/com/github/graycat27/twitterbot/web/WebController.java
+++ src/main/java/com/github/graycat27/twitterbot/web/WebController.java
@@ -3,17 +3,14 @@ package com.github.graycat27.twitterbot.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.security.Principal;
-import java.util.Locale;
 
 @Controller
 @SpringBootApplication
 public class WebController {
 
+    /** Spring boot用のmainメソッド */
     public static void main(String[] args) throws Exception {
         SpringApplication.run(WebController.class, args);
     }
