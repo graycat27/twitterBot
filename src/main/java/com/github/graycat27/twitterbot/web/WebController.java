ea8c5950cda0d965f4039dee0cea8062703a6178
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-07-28 16:09:07 +0900

    SpringBootApplication化および /indexのハンドリング改善

--- src/main/java/com/github/graycat27/twitterbot/web/WebController.java
+++ src/main/java/com/github/graycat27/twitterbot/web/WebController.java
@@ -1,5 +1,8 @@
 package com.github.graycat27.twitterbot.web;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,10 +10,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.security.Principal;
 import java.util.Locale;
 
+@Controller
+@SpringBootApplication
 public class WebController {
 
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(WebController.class, args);
+    }
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(Locale locale, Model model, Principal p){
+    String index(Locale locale, Model model, Principal p){
         model.addAttribute("sample", "this is sample");
 
         return "index";
