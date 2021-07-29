f939f448f4eec9a95f6f8bcde568404744353537
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-07-29 11:26:11 +0900

    fix cherry-pick compile error

--- src/main/java/com/github/graycat27/twitterbot/bot/job/IBatchJob.java
+++ src/main/java/com/github/graycat27/twitterbot/bot/job/IBatchJob.java
@@ -1,9 +1,6 @@
 package com.github.graycat27.twitterbot.bot.job;
 
-import com.github.graycat27.twitterbot.utils.TaskStartEndLogging;
-
 public interface IBatchJob {
 
-    @TaskStartEndLogging
     void run();
 }
