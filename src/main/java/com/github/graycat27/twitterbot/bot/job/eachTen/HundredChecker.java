df56473a642ef231d6fc12155ca879133667cecc
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-07-31 09:21:43 +0900

    未使用importの整理

--- src/main/java/com/github/graycat27/twitterbot/bot/job/eachTen/HundredChecker.java
+++ src/main/java/com/github/graycat27/twitterbot/bot/job/eachTen/HundredChecker.java
@@ -3,15 +3,8 @@ package com.github.graycat27.twitterbot.bot.job.eachTen;
 import com.github.graycat27.twitterbot.bot.BotTask;
 import com.github.graycat27.twitterbot.bot.job.AbstractJob;
 import com.github.graycat27.twitterbot.heroku.db.domain.BotUsersDomain;
-import com.github.graycat27.twitterbot.heroku.db.domain.TwitterRecordDomain;
-import com.github.graycat27.twitterbot.heroku.db.query.TwitterRecordQuery;
-import com.github.graycat27.twitterbot.twitter.api.caller.GetUserInfo;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.sql.Timestamp;
 import java.util.List;
-import java.util.Random;
 
 public class HundredChecker extends AbstractJob {
 
