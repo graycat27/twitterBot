655a9756285a33858ecd0a49e67715e4a120bab1
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-08-03 11:54:30 +0900

    v1API(POST-needAuth)の呼出し部品作成 ref https://n3104.hatenablog.com/entry/20101014/1287070373

--- src/main/java/com/github/graycat27/twitterbot/twitter/api/CallTwitterApi.java
+++ src/main/java/com/github/graycat27/twitterbot/twitter/api/CallTwitterApi.java
@@ -2,12 +2,14 @@ package com.github.graycat27.twitterbot.twitter.api;
 
 import com.github.graycat27.twitterbot.heroku.db.domain.TwitterAuthDomain;
 import com.github.graycat27.twitterbot.heroku.db.query.TwitterAuthQuery;
+import com.github.graycat27.twitterbot.twitter.api.oauth.GetOauthHeader;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.CookieSpecs;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
@@ -29,6 +31,38 @@ public class CallTwitterApi {
     }
 
     // method
+    public String callApiV1(URIBuilder callUrl){
+        HttpEntity entity;
+        String responseJsonStr = null;
+        try{
+            HttpClient httpClient =
+                    HttpClients.custom().setDefaultRequestConfig(
+                            RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build()
+                    ).build();
+            HttpPost httpPost = new HttpPost(callUrl.build());
+            httpPost.setHeader("Authorization", GetOauthHeader.getOauthHeader());
+            httpPost.setHeader("Content-Type", "application/json");
+
+            HttpResponse response = httpClient.execute(httpPost);
+            entity = response.getEntity();
+
+        }catch(URISyntaxException | IOException e){
+            System.out.println("Exception occurred while calling Twitter API v2");
+            System.out.println(e.getMessage());
+            System.out.println(callUrl);
+            throw new RuntimeException(e);
+        }
+
+        responseJsonStr = convertEntity2JsonStr(entity);
+
+        System.out.println("==ApiResponse==>>>>>");
+        System.out.println(responseJsonStr);
+        System.out.println("==ApiResponse==<<<<<");
+
+        return responseJsonStr;
+
+    }
+
     public String callApiV2(URIBuilder callUrl){
         HttpEntity entity;
         String responseJsonStr = null;
---
+++ src/main/java/com/github/graycat27/twitterbot/twitter/api/oauth/GetOauthHeader.java
@@ -0,0 +1,88 @@
+package com.github.graycat27.twitterbot.twitter.api.oauth;
+
+import com.github.graycat27.twitterbot.heroku.db.domain.TwitterAuthDomain;
+import com.github.graycat27.twitterbot.heroku.db.query.TwitterAuthQuery;
+import com.github.graycat27.twitterbot.twitter.api.ApiUrl;
+import org.springframework.http.HttpMethod;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class GetOauthHeader {
+
+    private GetOauthHeader(){ /* インスタンス化防止 */ }
+
+    public static String getOauthHeader(){
+        TwitterAuthQuery authQuery = new TwitterAuthQuery();
+        TwitterAuthDomain authInfo = authQuery.selectOne(null);
+
+        RequestDomain oauthRequest = new RequestDomain(
+                authInfo.getApiKey(), authInfo.getSecretKey(),
+                "", "", HttpMethod.POST,
+                ApiUrl.getRequestToken.url
+        );
+
+        SortedMap<String, String> oauthParam = new TreeMap<>();
+        oauthParam.put("oauth_consumer_key", oauthRequest.getConsumerKey());
+        oauthParam.put("oauth_signature_method", "HMAC-SHA1");
+        oauthParam.put("oauth_timestamp", String.valueOf( (int)(System.currentTimeMillis()/1000L) ));
+        oauthParam.put("oaut_nonce", String.valueOf(Math.random()));
+        oauthParam.put("oauth_version", "1.0");
+
+        // 署名(oauth_signature) の生成
+        try{
+            String paramStr = "";
+            for(Map.Entry<String, String> param : oauthParam.entrySet()){
+                paramStr += ("&" + param.getKey() + "=" + param.getValue());
+            }
+            paramStr = paramStr.substring(1);   //最初の&を削除
+
+            String baseText = oauthRequest.getMethod().toString()
+                    + "&" + urlEncode(oauthRequest.getUrlStr())
+                    + "&" + urlEncode(paramStr);
+
+            String signKey = urlEncode(oauthRequest.getConsumerSecret())
+                    + "&" + urlEncode(oauthRequest.getOauthTokenSecret());
+
+            SecretKeySpec signingKey = new SecretKeySpec(signKey.getBytes(), "HmacSHA1");
+            javax.crypto.Mac mac = Mac.getInstance(signingKey.getAlgorithm());
+            mac.init(signingKey);
+            byte[] rawHmac = mac.doFinal(baseText.getBytes());
+            String signature = Base64.getEncoder().encodeToString(rawHmac);
+
+            oauthParam.put("oauth_signature", signature);
+        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        // Authorization header の作成
+        StringBuilder paramStrBuilder = new StringBuilder();
+        for(Map.Entry<String, String> param : oauthParam.entrySet()){
+            paramStrBuilder.append(", ")
+                    .append(param.getKey()).append("=\"").append(urlEncode(param.getValue())).append("\"");
+        }
+        String paramStr = paramStrBuilder.substring(2);
+        String authHeader = "OAuth " + paramStr;
+
+        System.out.println("generated oauth header = " + authHeader);
+        return authHeader;
+    }
+
+    private static String urlEncode(String string) {
+        try {
+            return URLEncoder.encode(string, StandardCharsets.UTF_8.name());
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
