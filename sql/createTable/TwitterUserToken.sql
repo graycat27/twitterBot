195eb69bd87fe2ac767fc29d461a6c80d0cbef47
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-08-04 19:19:19 +0900

    テーブル定義作成

---
+++ sql/createTable/TwitterUserToken.sql
@@ -0,0 +1,8 @@
+CREATE TABLE twitter_user_token (
+    created_date timestamp DEFAULT CURRENT_TIMESTAMP,
+    update_date timestamp,
+    is_deleted boolean DEFAULT false,
+    tw_user_id text PRIMARY KEY REFERENCES bot_users (tw_user_id),
+    oauth_token text,
+    oauth_token_secret text
+)
