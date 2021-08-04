a88bbcb46fb197dbc90bb9bbebc374782452a5ab
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-08-04 20:46:58 +0900

    twitter_user_token表のjavaソース構築（Domainへの列定義漏れ）

--- src/main/java/com/github/graycat27/twitterbot/heroku/db/domain/TwitterUserTokenDomain.java
+++ src/main/java/com/github/graycat27/twitterbot/heroku/db/domain/TwitterUserTokenDomain.java
@@ -4,6 +4,10 @@ import com.google.gson.Gson;
 
 public class TwitterUserTokenDomain extends CommonDomain {
 
+    private final String twUserId;
+    public String getTwUserId(){
+        return twUserId;
+    }
     private final String oauthToken;
     public String getOauthToken(){
         return oauthToken;
@@ -14,9 +18,11 @@ public class TwitterUserTokenDomain extends CommonDomain {
     }
 
     public TwitterUserTokenDomain(
+            String twUserId,
             String oauthToken,
             String oauthTokenSecret
     ){
+        this.twUserId = twUserId;
         this.oauthToken = oauthToken;
         this.oauthTokenSecret = oauthTokenSecret;
     }
