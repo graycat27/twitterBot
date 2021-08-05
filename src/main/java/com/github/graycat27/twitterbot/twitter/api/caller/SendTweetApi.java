2237d2ac27d382ea4a604eac87f25d33a29245cc
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-08-05 09:19:37 +0900

    userAuthTokenに対応してみた

--- src/main/java/com/github/graycat27/twitterbot/twitter/api/caller/SendTweetApi.java
+++ src/main/java/com/github/graycat27/twitterbot/twitter/api/caller/SendTweetApi.java
@@ -4,6 +4,7 @@ import com.github.graycat27.twitterbot.heroku.db.domain.TwitterUserTokenDomain;
 import com.github.graycat27.twitterbot.heroku.db.query.TwitterUserTokenQuery;
 import com.github.graycat27.twitterbot.twitter.api.ApiManager;
 import com.github.graycat27.twitterbot.twitter.api.ApiUrl;
+import com.github.graycat27.twitterbot.twitter.api.response.data.AccessToken;
 import com.github.graycat27.twitterbot.twitter.api.response.data.RequestToken;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URIBuilder;
@@ -24,16 +25,17 @@ public class SendTweetApi {
         List<NameValuePair> postParam = new ArrayList<>();
         postParam.add(new BasicNameValuePair("status","#ツイート数えったー テストツイートです"));
 
-        RequestToken token = getTokenByUser(userId);
+        AccessToken token = getTokenByUser(userId);
         ApiManager.getApiCaller().callApiV1Post(uriBuilder, token, postParam);
 
     }
 
-    private static RequestToken getTokenByUser(String userId){
+    private static AccessToken getTokenByUser(String userId){
         TwitterUserTokenQuery tokenQuery = new TwitterUserTokenQuery();
         TwitterUserTokenDomain param = new TwitterUserTokenDomain(userId, null, null);
 
         TwitterUserTokenDomain resultDomain = tokenQuery.selectOne(param);
-        return new RequestToken(userId, resultDomain.getOauthToken(), resultDomain.getOauthTokenSecret());
+        return new AccessToken(resultDomain.getOauthToken(), resultDomain.getOauthTokenSecret(),
+                resultDomain.getTwUserId(), null);
     }
 }
