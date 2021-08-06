2348053c8045b621da719f11d4f5cdf858d587dd
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-08-06 23:18:04 +0900

    DB-I/Oのlogging準備

--- src/main/java/com/github/graycat27/twitterbot/heroku/db/query/QueryRunnable.java
+++ src/main/java/com/github/graycat27/twitterbot/heroku/db/query/QueryRunnable.java
@@ -1,7 +1,11 @@
 package com.github.graycat27.twitterbot.heroku.db.query;
 
 import com.github.graycat27.twitterbot.heroku.db.MybatisConfig;
+import com.github.graycat27.twitterbot.heroku.db.domain.IDbDomain;
+import com.github.graycat27.twitterbot.utils.ListUtil;
 import org.apache.ibatis.session.SqlSessionFactory;
+
+import java.util.List;
 
 public abstract class QueryRunnable implements IQuery {
 
@@ -15,6 +19,34 @@ public abstract class QueryRunnable implements IQuery {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    protected void logParamObject(IDbDomain domain){
+        if(domain == null){
+            System.out.println("== DB-I/O parameter is nothing ==>");
+            return;
+        }
+        System.out.println("== DB-I/O parameter ==>");
+        System.out.println(domain.toString());
+        System.out.println("====<");
+    }
+
+    protected void logResultObject(IDbDomain domain){
+        System.out.println("== DB-I/O result ==>");
+        System.out.println(domain.toString());
+        System.out.println("====<");
+    }
+
+    protected void logResultObject(int count){
+        System.out.println("== DB-I/O result ==>");
+        System.out.println(count);
+        System.out.println("====<");
+    }
+
+    protected void logResultObject(List<? extends IDbDomain> resultList){
+        System.out.println("== DB-I/O result ==>");
+        ListUtil.printList(resultList);
+        System.out.println("====<");
     }
 
 }
