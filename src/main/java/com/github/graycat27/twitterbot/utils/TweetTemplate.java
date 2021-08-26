1c16ecd6f8552ae519f4e0dea06fbca9222e2881
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-08-26 18:51:12 +0900

    add link to registered tweet

--- src/main/java/com/github/graycat27/twitterbot/utils/TweetTemplate.java
+++ src/main/java/com/github/graycat27/twitterbot/utils/TweetTemplate.java
@@ -7,6 +7,7 @@ public class TweetTemplate {
     public static final String tag = " #ツイート数えったー ";
 
     public static final String hundred = "今日(%s)に入っておよそ%dツイートしました！" + tag;
-    public static final String authed = "登録しました！毎日0時頃にツイート数をつぶやきます" + tag;
+    public static final String authed = "登録しました！毎日0時頃にツイート数をつぶやきます" + tag
+            + "登録はここから https://graycat27twitterbot.herokuapp.com/";
     public static final String minus = "（ツイ消し・RT先の消失等でマイナスになったようです）";
 }
